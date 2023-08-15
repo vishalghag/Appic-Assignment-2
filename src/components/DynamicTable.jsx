a96f58afc49db56fd917c4ad0f1cbd0d7bc6bac2
Vishal Ghag <vishal.ghag@i2econsulting.com>
2023-08-15 19:03:47 +0530

    Appic Assignment

--- src/components/DynamicTable.jsx
+++ src/components/DynamicTable.jsx
@@ -1,43 +1,102 @@
 import { useEffect, useState } from "react";
 import { HashLoader } from "react-spinners";
 import User from "./User";
-import {  ascendingDescending, fetchTableData} from "../utils";
+import {
+  ascendingDescending,
+  fetchTableData,
+  filterToAgeRange,
+  generateAgeRange,
+} from "../utils";
 import { employeeDataStatic } from "../static";
 import { ASC, DESC } from "../utils/constant";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowDown, faArrowUp, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowDown,
+  faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
+import Select from "react-select";
 
 const DynamicTable = () => {
   const [employeeData, setEmployeeData] = useState([]);
   const [loader, setLoader] = useState(false);
-  const [toggle,SetToggle] = useState(true);
+  const [toggle, SetToggle] = useState(true);
+  const [selectedAgeRange, setSelectedAgeRange] = useState(null);
+
+  const [consistentEmployeeData,setConsistentEmployeeData] = useState([])
 
   useEffect(() => {
     fetchInitialData();
   }, []);
 
+
   const fetchInitialData = () => {
-    setLoader(true)
-    const {data,status} = fetchTableData()
-    if(status === 200){
-      setEmployeeData(data)
+    setLoader(true);
+    const { data, status } = fetchTableData();
+    if (status === 200) {
+      setEmployeeData(data);
+      setConsistentEmployeeData(data)
+      // setConsistentEmployeeData(employeeDataStatic);
     }
-    // setEmployeeData([])
-    setEmployeeData(employeeDataStatic)
-    setLoader(false)
-  }
+    setEmployeeData([])
+    // setEmployeeData(employeeDataStatic);
+    setLoader(false);
+  };
+
+const handleSelectOnChange = (data) => {
+  setSelectedAgeRange(data);
+  setEmployeeData(filterToAgeRange(employeeDataStatic,data.value.min,data.value.max))
+}
 
   const renderTable = () => {
     return (
       <>
         <h2>Dynamic Table</h2>
+        <Select 
+          placeholder='Select Age Range'
+          value={selectedAgeRange}
+          onChange={handleSelectOnChange}
+          options={generateAgeRange(consistentEmployeeData)}
+          
+        />
         <table>
           <thead>
             <tr>
               <th>ID's</th>
               <th>Employee Name</th>
               <th>Age</th>
-              {toggle ? <th>Employee Salary <button onClick={() => ascendingDescending(employeeData,setEmployeeData,ASC,SetToggle)}><FontAwesomeIcon icon={faArrowUp}/></button> </th> : <th>Employee Salary <button onClick={() => ascendingDescending(employeeData,setEmployeeData,DESC,SetToggle)}><FontAwesomeIcon icon={faArrowDown}/></button> </th> }
+              {toggle ? (
+                <th>
+                  Employee Salary{" "}
+                  <button
+                    onClick={() =>
+                      ascendingDescending(
+                        employeeData,
+                        setEmployeeData,
+                        ASC,
+                        SetToggle
+                      )
+                    }
+                  >
+                    <FontAwesomeIcon icon={faArrowUp} />
+                  </button>{" "}
+                </th>
+              ) : (
+                <th>
+                  Employee Salary{" "}
+                  <button
+                    onClick={() =>
+                      ascendingDescending(
+                        employeeData,
+                        setEmployeeData,
+                        DESC,
+                        SetToggle
+                      )
+                    }
+                  >
+                    <FontAwesomeIcon icon={faArrowDown} />
+                  </button>{" "}
+                </th>
+              )}
             </tr>
           </thead>
           <tbody>
@@ -48,7 +107,13 @@ const DynamicTable = () => {
     );
   };
 
-  return loader ? <HashLoader size={40} color='#443C68'/> : !!employeeData.length ? renderTable() : <h2>Error While Fetching Data</h2>;
+  return loader ? (
+    <HashLoader size={40} color="#443C68" />
+  ) : !!employeeData.length ? (
+    renderTable()
+  ) : (
+    <h2>Error While Fetching Data</h2>
+  );
 };
 
 export default DynamicTable;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,61 @@
 import SecureAxios from "./configAxios/SecureAxios";
-import {ASC,DESC, EmployeeUrl} from "./constant"
+import { ASC, DESC, EmployeeUrl } from "./constant";
 
 export const fetchTableData = async () => {
   SecureAxios({
     method: "GET",
-    url:EmployeeUrl,
+    url: EmployeeUrl,
   })
     .then(({ data, status }) => {
-      return { data, status }
+      return { data, status };
     })
     .catch((error) => error);
 };
 
-export const ascendingDescending = (employeeData, setEmployeeData, operation,SetToggle) => {
-    let updatedEmployeeData = [...employeeData]; // Create a copy of the array
+export const ascendingDescending = (
+  employeeData,
+  setEmployeeData,
+  operation,
+  SetToggle
+) => {
+  let updatedEmployeeData = [...employeeData]; // Create a copy of the array
 
-    if (operation === ASC) {
-        updatedEmployeeData.sort((a, b) => a.employee_salary - b.employee_salary);
-        SetToggle(false)
-    } 
-    if (operation === DESC) {
-        updatedEmployeeData.sort((a, b) => b.employee_salary - a.employee_salary);
-        SetToggle(true)
-    }
+  if (operation === ASC) {
+    updatedEmployeeData.sort((a, b) => a.employee_salary - b.employee_salary);
+    SetToggle(false);
+  }
+  if (operation === DESC) {
+    updatedEmployeeData.sort((a, b) => b.employee_salary - a.employee_salary);
+    SetToggle(true);
+  }
 
-    setEmployeeData(updatedEmployeeData);
+  setEmployeeData(updatedEmployeeData);
 };
 
+export const filterToAgeRange = (employeeData, min, max) =>
+  employeeData.filter(
+    (number) => number.employee_age >= min && number.employee_age <= max
+  );
+
+export const generateAgeRange = (employeeData) => {
+  const maxAge = Math.max(
+    ...employeeData.map((ele) => parseInt(ele.employee_age))
+  );
+  const rangeCount = Math.ceil(maxAge / 20);
+  const ranges = [];
+
+  for (let i = 0; i < rangeCount; i++) {
+    const minAge = i * 20;
+    const maxAge = minAge + 20;
+    const datum = {
+      value: {
+        min: minAge,
+        max: maxAge,
+      },
+      label: `${minAge}-${maxAge}`,
+    };
+    ranges.push(datum);
+  }
+
+  return ranges;
+};
